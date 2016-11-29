71bc6d4313856ccf9b327223e47e2848a9a8bdba
jubayed <hmjubayed@gmail.com>
2016-11-30 01:36:47 +0600

    check database connection

--- src/installer/config.java
+++ src/installer/config.java
@@ -5,10 +5,34 @@
  */
 package installer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  *
  * @author jubayed
  */
 public class config {
     
+    static boolean saveDatabaseInfo(String DB_DATABASE, String DB_USERNAME, String DB_PASSWORD, String DB_HOST, String DB_TABLEPREFIX) {
+        boolean result = false;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://" + DB_HOST + ":3306/" + DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+            //here sonoo is database name, root is username and password  
+            Statement stmt = con.createStatement();
+            result = true;
+            con.close();
+            con=null;
+            stmt=null;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("conntion status= " + result);
+      
+        return result;
+    }
+    
+    //===========================================================
 }
